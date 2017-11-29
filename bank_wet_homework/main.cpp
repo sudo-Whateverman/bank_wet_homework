35b7795ba9f6115618ff715aa1849f8fde28aa7e
sudo-Whateverman <nick@smartgreen.co.il>
2017-11-29 22:50:45 +0200

    check cmd args.

--- bank_wet_homework/main.cpp
+++ bank_wet_homework/main.cpp
@@ -22,7 +22,38 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    int N = atoi(argv[0]); // number of ATMs
+//    printf("This is a homework\n");
+    
+//     Validate input
+    int N;
+//     printf("%d\n",argc);
+//    for (int i=0; i<argc; i++)
+//    {
+//        printf("%s\n",argv[i]);
+//    }
+    try
+    {
+        N = stoi(argv[1]); // number of ATMs - the first arg, the zeroth is the filename
+    }
+    catch(std::invalid_argument& e){
+        // if no conversion could be performed
+        printf("First argument should be a number\n");
+        exit(-1);
+    }
+    if (N!=argc-2) // Since the first arg is the N_atm, and last is \0 we deduct 2
+    {
+        printf("Please supply the correct number of arguments\n");
+        exit(-1);
+    }
+    for (int i=2; i<argc; i++)
+    {
+        if( access( argv[i], R_OK ) == -1 ) {
+            // file doesn't exists
+            printf("The file specified isn't found\n");
+            exit(-1);
+        } 
+    }
+    
     pthread_t threads;
     for(int i=0; i<N; i++){
         atm atm(argv[i]); // insert filename;
